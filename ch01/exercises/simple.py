58faed60b3a5d88572557ee0625f02c2a75ded33
Jesse Fonte <112440093+JesseFonte@users.noreply.github.com>
2022-09-06 22:05:28 +0000

    ch01 exercise

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,18 @@
+print (10*5) 
+print (10**2)
+print (15/10)
+print (15//10)
+print (-15//10)
+print (15%10)
+print (10%15)
+print (10%10)
+print (0%10) 
+print (10/15) 
+# the last number is wrong because technically it is infinite yet when inputted it ends
+print("What is the current exchange rate for the Euro to a US Dollar?")
+rate = int(input("enter something:"))
+print("How much would you like to exchange?")
+amount = int(input())
+total = amount * rate
+result = total - 3
+print(result)
