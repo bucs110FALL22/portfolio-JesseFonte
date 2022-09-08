cb303c837fa9bb086542edf4a727686107bf5e1b
Jesse Fonte <112440093+JesseFonte@users.noreply.github.com>
2022-09-08 23:17:00 +0000

    rando

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,17 @@
-
+import random 
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
+classes_per_week = 3
+print(classes_per_week,type(classes_per_week))
+cost_per_class = (cost_per_week/classes_per_week)
+print(cost_per_class, type(cost_per_class))
+print("Classes only cost:", cost_per_class)
 
 #Part B
+my_list = ("Frank","Kanye","Tyler","Earl","Donald")
+rando = random.choice(my_list)
+print(rando)
