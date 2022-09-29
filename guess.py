5a73be5ed054396ffcd8189f47106b4a17035211
JesseFonte <112440093+JesseFonte@users.noreply.github.com>
2022-09-28 20:42:24 -0400

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,29 @@
+import random 
+
+random = random.randrange(1,11)
+
+
+for i in range(3): 
+  guess = int(input("What is your guess?"))
+  if (guess == random):
+    print ("correct!")
+    break
+  
+  elif (guess < random):
+    print ("Too Low")
+
+  elif (guess > random):
+    print ("Too High") 
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+ 
+>>>>>>> origin/main
+=======
+ 
+>>>>>>> origin/main
+=======
+ 
+>>>>>>> origin/main
