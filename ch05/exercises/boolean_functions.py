9c57765a68e6334f4a0fc6cfb8918fc5e74eba81
Jesse Fonte <112440093+JesseFonte@users.noreply.github.com>
2022-10-18 03:44:35 +0000

    vdov

---
+++ ch05/exercises/boolean_functions.py
@@ -0,0 +1,13 @@
+def percentage_to_letter(score):
+  if score >= 90:
+    return True 
+  elif score >90 and score <= 70:
+    return True 
+  elif score > 70 and score <= 60:
+    return True 
+  else:
+    return False
+
+score = int(input("What is your score:"))
+print(percentage_to_letter(score))
+                 
