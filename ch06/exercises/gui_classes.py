7abc74ee89eb23ae44306bc8274b2f1d2bd36a54
Jesse Fonte <112440093+JesseFonte@users.noreply.github.com>
2022-10-24 21:52:11 +0000

    kjb KSJbsc

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,17 @@
+class Player:
+	def __init__(self):
+      self.power_up = True 
+      self.grows = True   
+      self.doubles 
+
+class Block:
+    def __init__(self):
+      if self.hit = True: 
+        self.breakable = True
+        self.break 
+
+class Enemy:
+  def __init__(self):
+    self.jumped_on = True 
+    self.break = True 
+    self.break 
